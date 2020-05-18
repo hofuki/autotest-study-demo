680d240ee2b03a636de1f60946b2f684cad02fdb
彭风贵 <1834332273@qq.com>
2020-05-18 15:23:27 +0800

    首次提交all file

---
+++ src/baidu_ui/const.py
@@ -0,0 +1,2 @@
+# coding = utf-8
+EXCEL_DIR = "D:/demo/data/query.xlsx"
---
+++ src/httpin_api/const.py
@@ -0,0 +1,5 @@
+# coding = utf-8
+IP_URL = "/ip"
+POST_URL = "/post"
+BASE_URL = "http://httpbin.org"
+LOCAL_IP = "183.220.46.19"
