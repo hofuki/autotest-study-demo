5e2fccc88bfe85d6615311a1db801b376bffac70
hofuki <65529428+hofuki@users.noreply.github.com>
2020-05-19 11:44:41 +0800

    Update const.py

--- src/httpin_api/const.py
+++ src/httpin_api/const.py
@@ -1,7 +1,8 @@
 # coding = utf-8
-import socket
-hostname = socket.gethostname()
+#import socket
+#hostname = socket.gethostname()
 IP_URL = "/ip"
 POST_URL = "/post"
 BASE_URL = "http://httpbin.org"
-LOCAL_IP = socket.gethostbyname(hostname)
+LOCAL_IP = "117.175.187.184"
+#LOCAL_IP = socket.gethostbyname(hostname)
